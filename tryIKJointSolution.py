6bdab80380aa54d495e94d8616f6af05cdd2ff81
Minghe <u2g8z7@u.northwestern.edu>
2015-11-30 10:03:44 -0600

    try IK

---
+++ tryIKJointSolution.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import argparse
+import struct
+import sys
+
+import rospy
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+import baxter_interface
+
+from baxter_interface import CHECK_VERSION
+def ik_test(limb):
+    
+    ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    ikreq = SolvePositionIKRequest()
+    hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+    poses = {
+        'left': PoseStamped(
+            header=hdr,
+            pose=Pose(
+                position=Point(
+                    x=0.657579481614,
+                    y=0.851981417433,
+                    z=0.0388352386502,
+                ),
+                orientation=Quaternion(
+                    x=-0.366894936773,
+                    y=0.885980397775,
+                    z=0.108155782462,
+                    w=0.262162481772,
+                ),
+            ),
+        ),
+        'right': PoseStamped(
+            header=hdr,
+            pose=Pose(
+                position=Point(
+                    x=0.656982770038,
+                    y=-0.852598021641,
+                    z=0.0388609422173,
+                ),
+                orientation=Quaternion(
+                    x=0.367048116303,
+                    y=0.885911751787,
+                    z=-0.108908281936,
+                    w=0.261868353356,
+                ),
+            ),
+        ),
+    }
+
+    ikreq.pose_stamp.append(poses[limb])
+    try:
+        rospy.wait_for_service(ns, 5.0)
+        resp = iksvc(ikreq)
+    except (rospy.ServiceException, rospy.ROSException), e:
+        rospy.logerr("Service call failed: %s" % (e,))
+        return 1
+
+    # Check if result valid, and type of seed ultimately used to get solution
+    # convert rospy's string representation of uint8[]'s to int's
+    resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
+                               resp.result_type)
+    if (resp_seeds[0] != resp.RESULT_INVALID):
+        seed_str = {
+                    ikreq.SEED_USER: 'User Provided Seed',
+                    ikreq.SEED_CURRENT: 'Current Joint Angles',
+                    ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+                   }.get(resp_seeds[0], 'None')
+        print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
+              (seed_str,))
+        # Format solution into Limb API-compatible dictionary
+        limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+        print "\nIK Joint Solution:\n", limb_joints
+        print "------------------"
+        print "Response Message:\n", resp
+    else:
+        print("INVALID POSE - No Valid Joint Solution Found.")
+    # set arm joint positions to solution
+    arm = baxter_interface.Limb(limb)
+    while not rospy.is_shutdown():
+        arm.set_joint_positions(limb_joints)
+        rospy.sleep(0.01)
+    return 0
+
+
+def main():
+
+    arg_fmt = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
+                                     description=main.__doc__)
+    parser.add_argument(
+        '-l', '--limb', choices=['left', 'right'], required=True,
+        help="the limb to test"
+    )
+    args = parser.parse_args(rospy.myargv()[1:])
+    print("Initializing node... ")
+    rospy.init_node("try1")
+    print("Getting robot state... ")
+    rs = baxter_interface.RobotEnable(CHECK_VERSION)
+    init_state = rs.state().enabled
+    
+    def clean_shutdown():
+        print("\nExiting example...")
+        if not init_state:
+            print("Disabling robot...")
+            rs.disable()
+    rospy.on_shutdown(clean_shutdown)    
+    
+    print("Enabling robot... ")
+    rs.enable()
+    
+    ik_test(args.limb)
+    
+    
+
+if __name__ == '__main__':
+    main()
