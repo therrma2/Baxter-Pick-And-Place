67b43efa6dca88f74c83a8e1183b4c14380601d5
Tim Herrmann <therrma2@gmail.com>
2015-12-07 10:40:38 -0600

    updated right cam script.  rcam node now outputs a Point msg type where the x and y values are the x,y coordinates of the detected block, and the z value is the slope of a line drawn between the two vertices with the largest y values.  The movement node can use this slope to calcuate a joint angle for the gripper.

--- scripts/rightcam.py
+++ scripts/rightcam.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 from std_msgs.msg import UInt16
+from geometry_msgs.msg import Point
 
 import baxter_interface as bi
 
@@ -20,7 +21,7 @@ def node1cb(data, args ):
        frame = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError, e:
         print("==[CAMERA MANAGER]==", e)
-    cv2.imshow('frame',frame)
+    #cv2.imshow('frame',frame)
     cv2.waitKey(25)
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -53,26 +54,53 @@ def node1cb(data, args ):
         rect = cv2.minAreaRect(c)
         ((x,y),radius) =cv2.minEnclosingCircle(c)
         box = cv2.cv.BoxPoints(rect)
+        print box
         box = np.int0(box)
         M = cv2.moments(c)
         try:
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         except ZeroDivisonError:
             return
+        #print "--------------------------------------------------------"
 
-        
-        print box
+        #print "box0", box[0]
+        #print "box1", box[1]
+        #print "box2", box[2]
+        #print "box3", box[3]
 
         if radius > 20:
             cv2.drawContours(frame,[box],0,(0,255,255),2)
             cv2.circle(mask, center, 5, (0, 0, 255), -1)
 
-        #print(x,y)
+        box2 = [[box[0][0],box[0][1]],[box[1][0],box[1][1]],[box[2][0],box[2][1]],[box[3][0],box[3][1]]]
+        #print box2
+        boxx = [box2[0][0],box2[1][0],box2[2][0],box2[3][0]]
+        boxy = [box2[0][1],box2[1][1],box2[2][1],box2[3][1]]
+        
+        index1 = boxy.index(max(boxy))
+        
+        pt1 = box2.pop(index1)
+        #print "pt1", pt1
+        boxx = [box2[0][0],box2[1][0],box2[2][0]]
+        boxy = [box2[0][1],box2[1][1],box2[2][1]]
+        
+        index2 = boxy.index(max(boxy))
 
+        pt2 = box2.pop(index2)
+        #print "pt2",pt2
+        #print(x,y)
+        
+        pt1 = (pt1[0],pt1[1])
+        pt2 = (pt2[0],pt2[1])
+
+        cv2.line(frame,pt1,pt2,(0,0,255),thickness=3)
+        slope = float((float(pt2[1])-float(pt1[1]))/(float(pt2[0])-float(pt1[0])))
+        
         x_int = int(x)
         y_int = int(y)
-        msg = UInt16()
-        msg.data = x_int
+        
+        msg = Point(x,y,slope)
+
         pub.publish(msg)    
         
 
@@ -87,7 +115,7 @@ def nothing(self):
 
 def node1():
     
-    rospy.init_node("rightcam")
+    rospy.init_node("rcam")
     bridge = CvBridge()
     rightcam = bi.CameraController('right_hand_camera')
         
@@ -95,18 +123,17 @@ def node1():
 
     cv2.namedWindow ('filter')
 
-    cv2.createTrackbar('HMin','filter',59,255,nothing)
-    cv2.createTrackbar('SMin','filter',77,255,nothing)
-    cv2.createTrackbar('VMin','filter',130,255,nothing)
+    cv2.createTrackbar('HMin','filter',31,255,nothing)
+    cv2.createTrackbar('SMin','filter',85,255,nothing)
+    cv2.createTrackbar('VMin','filter',118,255,nothing)
 
-    cv2.createTrackbar('HMax','filter',90,255,nothing)
-    cv2.createTrackbar('SMax','filter',136,255,nothing)
-    cv2.createTrackbar('VMax','filter',187,255,nothing)
+    cv2.createTrackbar('HMax','filter',107,255,nothing)
+    cv2.createTrackbar('SMax','filter',255,255,nothing)
+    cv2.createTrackbar('VMax','filter',255,255,nothing)
 
 
-    pub = rospy.Publisher('node1pub', UInt16, queue_size=10)
+    pub = rospy.Publisher('rcampub', Point, queue_size=10)
     rospy.Subscriber('/cameras/right_hand_camera/image', Image, node1cb, callback_args=(bridge, pub))
-
     
     print 'here before spin'
     rospy.spin()
